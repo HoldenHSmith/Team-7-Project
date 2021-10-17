42acc405b4f03d7ef343992c628f5d3f4b5cd434
Jayden <jaydenghunter@outlook.com>
2021-10-17 13:38:13 +1100

    Created Alertness level script, added material block properties

---
+++ Team Seven Project/Assets/Scripts/Prototyping/TestGuardDistract.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestGuardDistract : MonoBehaviour
+{
+	public float evaluation;
+	public float speed;
+	private Renderer _renderer;
+	private MaterialPropertyBlock _propBlock;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+		_propBlock = new MaterialPropertyBlock();
+		_renderer = GetComponent<Renderer>();
+    }
+
+	private void Update()
+	{
+		//Get the current value of the material properties in the renderer.
+		_renderer.GetPropertyBlock(_propBlock);
+		//Assign our new value
+		_propBlock.SetFloat("_Evaluation", evaluation);
+		_propBlock.SetFloat("_Speed", speed);
+		_renderer.SetPropertyBlock(_propBlock);
+	}
+
+}
