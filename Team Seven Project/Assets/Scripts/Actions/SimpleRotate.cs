6f61c6273722c094db2c6be4b712c845e736d75b
Jayden <jaydenghunter@outlook.com>
2021-10-18 20:05:39 +1100

    Added Rotation, Detection View Masks, Dither Shader, Added Utils Class

---
+++ Team Seven Project/Assets/Scripts/Actions/SimpleRotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SimpleRotate : MonoBehaviour
+{
+	[SerializeField] float m_RotationSpeed = 1;
+	[SerializeField] float m_RotationAngleYStart = 45;
+	[SerializeField] float m_RotationAngleYFinish = 45;
+
+	private void Update()
+	{
+		float rY = Mathf.SmoothStep(m_RotationAngleYStart, m_RotationAngleYFinish, Mathf.PingPong(Time.time * m_RotationSpeed, 1));
+
+		transform.rotation = Quaternion.Euler(0, rY, 0);
+	}
+}
