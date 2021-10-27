2075227bf4a74cb797ff927aed0d628e2fbe2722
Jayden <jaydenghunter@outlook.com>
2021-10-27 17:28:14 +1100

    Added Point Sampler, Sample Points, Modified Recipient Handler

---
+++ Team Seven Project/Assets/Scripts/Player/PointSampler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PointSampler : MonoBehaviour
+{
+	[SerializeField] private List<Transform> _samplePoints = new List<Transform>();
+	[SerializeField] private bool _hideSamplePoints = true;
+
+	private void Awake()
+	{
+		if (_hideSamplePoints)
+		{
+			foreach (Transform samplePoint in _samplePoints)
+			{
+				samplePoint.GetComponent<MeshRenderer>().enabled = false;
+			}
+		}
+	}
+
+	public List<Transform> SamplePoints { get => _samplePoints; }
+}
--- Team Seven Project/Assets/Scripts/Telegram/RecipientHandler.cs
+++ Team Seven Project/Assets/Scripts/Telegram/RecipientHandler.cs
@@ -11,6 +11,19 @@ public class RecipientHandler : MonoBehaviour
 	[SerializeField, TypeConstraint(typeof(IMessageReceiver))]
 	private List<GameObject> _recipients = new List<GameObject>();
 
+	private List<IMessageReceiver> _recipientInterfaceList = new List<IMessageReceiver>();
+
+	private void Awake()
+	{
+		for (int i = 0; i < _recipients.Count; i++)
+		{
+			IMessageReceiver recipient = _recipients[i].GetComponent<IMessageReceiver>();
+			_recipientInterfaceList.Add(recipient);
+		}
+	}
+
+	public List<IMessageReceiver> Recipients { get => _recipientInterfaceList; }
+
 #if UNITY_EDITOR
 	private void OnDrawGizmosSelected()
 	{
