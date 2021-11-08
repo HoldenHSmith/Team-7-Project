b1849f3f1403a159d49faaf38b6da0add62af47b
Jayden <jaydenghunter@outlook.com>
2021-11-08 15:12:47 +1100

    Started the note system

---
+++ Team Seven Project/Assets/Scripts/Interactables/PaperNote.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PaperNote : MonoBehaviour, ICollectable, IInteractable
+{
+
+    [SerializeField] private string _noteText;
+
+    public void OnCollect()
+    {
+        CollectionManager.Instance.SetNotevalue(this, true);
+    }
+
+    public void OnInteract(PlayerCharacter playerCharacter)
+    {
+        OnCollect();
+        this.gameObject.SetActive(false);
+        //Open up note on canvas UI and set text
+    }
+}
--- Team Seven Project/Assets/Scripts/Management/CollectionManager.cs
+++ Team Seven Project/Assets/Scripts/Management/CollectionManager.cs
@@ -3,33 +3,44 @@ using UnityEngine;
 
 public sealed class CollectionManager
 {
-	private static readonly CollectionManager _instance = new CollectionManager();
+    private static readonly CollectionManager _instance = new CollectionManager();
 
-	private Dictionary<AreaType, bool> _keysCollected;
+    private Dictionary<AreaType, bool> _keysCollected;
+    private Dictionary<PaperNote, bool> _notesCollected;
 
-	public CollectionManager()
-	{
-		_keysCollected = new Dictionary<AreaType, bool>();
 
-		for (int i = 0; i < (int)AreaType.Count; i++)
-		{
-			_keysCollected.Add((AreaType)i, false);
-		}
+    public CollectionManager()
+    {
+        _keysCollected = new Dictionary<AreaType, bool>();
 
-	}
+        for (int i = 0; i < (int)AreaType.Count; i++)
+        {
+            _keysCollected.Add((AreaType)i, false);
+        }
 
-	public void SetKeyValue(AreaType area, bool value)
-	{
-		_keysCollected[area] = value;
-		Debug.Log($"Key Collected: {area}");
-	}
+    }
 
-	public bool CheckKeyCollected(AreaType area)
-	{
-		return _keysCollected[area];
-	}
+    public void SetKeyValue(AreaType area, bool value)
+    {
+        _keysCollected[area] = value;
+        if (value)
+            Debug.Log($"Key Collected: {area}");
+    }
 
-	public static CollectionManager Instance { get => _instance; }
-	public Dictionary<AreaType, bool> KeysCollected { get => _keysCollected; }
-	
+    public void SetNotevalue(PaperNote note, bool value)
+    {
+        _notesCollected[note] = value;
+        if (value)
+            Debug.Log($"Note Collected!");
+    }
+
+    public bool CheckKeyCollected(AreaType area)
+    {
+        return _keysCollected[area];
+    }
+
+    public static CollectionManager Instance { get => _instance; }
+    public Dictionary<AreaType, bool> KeysCollected { get => _keysCollected; }
+    public Dictionary<PaperNote, bool> NotesCollected { get => _notesCollected; }
+
 }
