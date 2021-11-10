89bcfe78fc80bf3b1eca9320fa875cdc0fcb010f
Jayden <jaydenghunter@outlook.com>
2021-11-10 12:23:02 +1100

    Added Falloff support for sound emissions, modified guard investigation behaviour

--- Team Seven Project/Assets/Scripts/Detection/AudioDetection.cs
+++ Team Seven Project/Assets/Scripts/Detection/AudioDetection.cs
@@ -3,98 +3,103 @@ using UnityEngine;
 
 public class AudioDetection : MonoBehaviour, IMessageSender
 {
-	[SerializeField] private float _detectionRange = 10.0f;
-	[SerializeField] private float _alertnessReductionPerSecond = 2.5f;
-	[SerializeField] private float _startReducingCooldown = 1.0f;
-	[SerializeField] private float _alertnessThreshold = 100;
+    [SerializeField] private float _detectionRange = 10.0f;
+    [SerializeField] private float _alertnessReductionPerSecond = 2.5f;
+    [SerializeField] private float _startReducingCooldown = 1.0f;
+    [SerializeField] private float _alertnessThreshold = 100;
 
-	private float _alertness = 0;
-	private float _reductionCooldownTimer;
-	private LayerMask _checkLayer;
+    private float _alertness = 0;
+    private float _reductionCooldownTimer;
+    private LayerMask _checkLayer;
 
-	public void SendMessage()
-	{
+    public void SendMessage()
+    {
 
-	}
+    }
 
-	private void Awake()
-	{
-		_checkLayer = (LayerMask.NameToLayer("Wall"));
-	}
+    private void Awake()
+    {
+        _checkLayer = (LayerMask.NameToLayer("Wall"));
+    }
 
-	private void Update()
-	{
-		if (_alertness > 0)
-			_reductionCooldownTimer -= Time.deltaTime;
+    private void Update()
+    {
+        if (_alertness > 0)
+            _reductionCooldownTimer -= Time.deltaTime;
 
-		if (_reductionCooldownTimer <= 0 && _alertness > 0)
-		{
-			_alertness -= _alertnessReductionPerSecond * Time.deltaTime;
-		}
+        if (_reductionCooldownTimer <= 0 && _alertness > 0)
+        {
+            _alertness -= _alertnessReductionPerSecond * Time.deltaTime;
+        }
 
-		if (_alertness < 0)
-			_alertness = 0;
-		if (_alertness > _alertnessThreshold)
-			_alertness = _alertnessThreshold;
-	}
+        if (_alertness < 0)
+            _alertness = 0;
+        if (_alertness > _alertnessThreshold)
+            _alertness = _alertnessThreshold;
+    }
 
-	public bool ThresholdReached()
-	{
-		if (_alertness >= _alertnessThreshold)
-			return true;
-		else
-			return false;
-	}
+    public bool ThresholdReached()
+    {
+        if (_alertness >= _alertnessThreshold)
+            return true;
+        else
+            return false;
+    }
 
-	public bool ProcessSound(SoundEmission sound)
-	{
-		float distance = Vector3.Distance(transform.position, sound.Position);
+    public bool ProcessSound(SoundEmission sound)
+    {
+        float distance = Vector3.Distance(transform.position, sound.Position);
 
-		if (distance <= _detectionRange)
-		{
-			Vector3 direction = sound.Position - transform.position;
-			RaycastHit hit;
+        if (distance <= _detectionRange)
+        {
+            Vector3 direction = sound.Position - transform.position;
+            RaycastHit hit;
 
-			Debug.DrawRay(transform.position, direction, Color.blue, 1);
-			DebugEx.DrawHitMark(sound.Position, Color.white);
-			if (Physics.Raycast(transform.position, direction, out hit))
-			{
-				float distanceToSound = Vector3.Distance(transform.position, sound.Position);
-				float distanceToRayHit = Vector3.Distance(transform.position, hit.point);
 
-				if (hit.collider.gameObject.layer != _checkLayer )
-				{
-					HeardSound(sound.Volume);
-					return true;
-				}
-				else if(distanceToRayHit > distanceToSound)
-				{
-					HeardSound(sound.Volume);
-					return true;
-				}
-			}
-			else
-			{
-				HeardSound(sound.Volume);
-				return true;
-			}
+            Debug.DrawRay(transform.position, direction, Color.blue, 1);
+            DebugEx.DrawHitMark(sound.Position, Color.white);
+            if (Physics.Raycast(transform.position, direction, out hit))
+            {
+                float distanceToSound = Vector3.Distance(transform.position, sound.Position);
+                float distanceToRayHit = Vector3.Distance(transform.position, hit.point);
+                float heardVolume = sound.Volume;
+                if (sound.DistanceFalloff != 0)
+                    heardVolume -= (distance /sound.DistanceFalloff) * sound.Volume;
 
-			return false;
-		}
+                Debug.Log($"Volume Heard: {heardVolume}");
+                if (hit.collider.gameObject.layer != _checkLayer)
+                {
+                    HeardSound(heardVolume);
+                    return true;
+                }
+                else if (distanceToRayHit > distanceToSound)
+                {
+                    HeardSound(sound.Volume);
+                    return true;
+                }
+            }
+            else
+            {
+                HeardSound(sound.Volume);
+                return true;
+            }
 
-		return false;
-	}
+            return false;
+        }
 
-	private void HeardSound(float volume)
-	{
-		_alertness += volume;
-		_reductionCooldownTimer = _startReducingCooldown;
-	}
+        return false;
+    }
 
-	private void OnDrawGizmosSelected()
-	{
-		DebugEx.DrawCircle(transform.position, _detectionRange, Color.yellow);
-	}
+    private void HeardSound(float volume)
+    {
+        _alertness += volume;
+        _reductionCooldownTimer = _startReducingCooldown;
+    }
 
-	public float Alertness { get => _alertness; set => _alertness = value; }
+    private void OnDrawGizmosSelected()
+    {
+        DebugEx.DrawCircle(transform.position, _detectionRange, Color.yellow);
+    }
+
+    public float Alertness { get => _alertness; set => _alertness = value; }
 }
