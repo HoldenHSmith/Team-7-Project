ba3e3c34f963c5d115f26f4f5c6ba5b09f26a8c1
Jayden <jaydenghunter@outlook.com>
2021-11-24 12:25:58 +1100

    Credits Scene Set up

---
+++ Team Seven Project/Assets/Scripts/Menus and UI/CreditsHandler.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CreditsHandler : MonoBehaviour
+{
+	[SerializeField] private TextMeshProUGUI _title;
+	[SerializeField] private TextMeshProUGUI _name;
+	[SerializeField] private AudioClip _scanClip = null;
+	private AudioSource _audioSource;
+
+	private int _currentName = 0;
+	private int _subjectNumber = 3023;
+
+	private string[] _titles =
+	{
+		"PRODUCER",
+		"LEAD DESIGNER",
+		"DESIGNER",
+		"LEAD PROGRAMMER",
+		"LEAD ARTIST",
+		"ARTIST",
+		"ARTIST",
+		"ARTIST",
+		"ARTIST",
+		"SPECIAL THANKS",
+		"SPECIAL THANKS",
+		"SPECIAL THANKS",
+	};
+	private string[] _names =
+{
+		"HOLDEN SMITH",
+		"FLYNN VAN STRYP",
+		"ZAC REICHELT",
+		"JAYDEN HUNTER",
+		"BEN LINDRIGE",
+		"ALLAN TRAN",
+		"ANGELIKA RYE",
+		"KENT SUTER",
+		"SCOTT HANCOCK",
+		"STEVEN VANDERGRAAF",
+		"AIE MELBOURNE",
+		"USER TESTERS"
+	};
+
+	private void Awake()
+	{
+		_title.text = "";
+		_name.text = "";
+		_audioSource = GetComponent<AudioSource>();
+	}
+
+	public void NextName()
+	{
+		_audioSource.PlayOneShot(_scanClip);
+		if (_currentName < _names.Length)
+		{
+			_title.text = _titles[_currentName];
+			_name.text = _names[_currentName];
+			_currentName++;
+		}
+		else
+		{
+			_title.text = "";
+			_name.text = $"SUBJECT #{_subjectNumber++}";
+		}
+	}
+}
